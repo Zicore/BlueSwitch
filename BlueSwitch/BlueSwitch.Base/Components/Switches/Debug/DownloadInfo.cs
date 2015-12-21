d66c3e261631c83e96d467c14d30fa9fe92ba683
Andre Vitt <vitt@opdenhoff.de>
2015-12-21 14:44:03 +0000

    Blue Switch Test Download

---
+++ BlueSwitch/BlueSwitch.Base/Components/Switches/Debug/DownloadInfo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlueSwitch.Base.Components.Switches.Debug
+{
+    public class DownloadInfo
+    {
+        public int Status { get; set; }
+        public bool IsDownloading { get; set; }
+
+        public override string ToString()
+        {
+            return $"Status: {Status}";
+        }
+    }
+}
---
+++ BlueSwitch/BlueSwitch.Base/Components/Switches/Debug/DownloadSimulatorSwitch.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using BlueSwitch.Base.Components.Base;
+using BlueSwitch.Base.Components.Switches.Base;
+using BlueSwitch.Base.Components.Types;
+using BlueSwitch.Base.Processing;
+
+namespace BlueSwitch.Base.Components.Switches.Debug
+{
+    public class DownloadSimulatorSwitch : SwitchBase
+    {
+        public override GroupBase OnSetGroup()
+        {
+            return GroupBase.Debug;
+        }
+
+        protected override void OnInitialize(Engine renderingEngine)
+        {
+            base.OnInitialize(renderingEngine);
+
+            Name = "DownloadTest";
+
+            AddInput(new ActionSignature());
+            AddOutput(new ActionSignature());
+
+            AddOutput(typeof(int));
+        }
+
+        private DownloadInfo info;
+
+        protected override void OnProcess<T>(Processor p, ProcessingNode<T> node)
+        {
+            if (info == null)
+            {
+                info = new DownloadInfo();
+            }
+
+            if (!info.IsDownloading)
+            {
+                info.IsDownloading = true;
+
+                Task.Factory.StartNew(() =>
+                {
+                    while (info.IsDownloading)
+                    {
+                        Thread.Sleep(500);
+                        info.Status++;
+                        if (info.Status >= 100)
+                        {
+                            info.Status = 100;
+                            info.IsDownloading = false;
+                        }
+
+                    }
+                });
+            }
+            base.OnProcess(p, node);
+        }
+
+        protected override void OnProcessData<T>(Processor p, ProcessingNode<T> node)
+        {
+            SetData(1, new DataContainer(info));
+            base.OnProcessData(p, node);
+        }
+    }
+}
