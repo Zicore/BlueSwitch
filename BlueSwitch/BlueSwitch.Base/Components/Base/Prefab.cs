c79efe8ff1e00d2459cdeeae81f729836c79aea7
Zicore <zicore@gmx.de>
2016-12-30 19:54:05 +0100

    add basic code for prefab feature

---
+++ BlueSwitch/BlueSwitch.Base/Components/Base/Prefab.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BlueSwitch.Base.Components.Switches.Base;
+using Newtonsoft.Json;
+
+namespace BlueSwitch.Base.Components.Base
+{
+    public class Prefab
+    {
+        [JsonProperty("Items")]
+        public List<SwitchBase> Items { get; set; } = new List<SwitchBase>();
+
+        [JsonProperty("Connections")]
+        public List<Connection> Connections { get; set; } = new List<Connection>();
+    }
+}
---
+++ BlueSwitch/BlueSwitch.Base/Components/Switches/Meta/PrefabSwitch.cs
@@ -0,0 +1,47 @@
+﻿using System.Linq;
+using BlueSwitch.Base.Components.Base;
+using BlueSwitch.Base.Components.Switches.Base;
+
+namespace BlueSwitch.Base.Components.Switches.Meta
+{
+    public class PrefabSwitch : SwitchBase
+    {
+        public Prefab Prefab { get; set; } = new Prefab();
+
+        protected override void OnInitialize(Engine renderingEngine)
+        {
+            base.OnInitialize(renderingEngine);
+            AutoDiscoverDisabled = true;
+
+            LoadPrefab();
+        }
+
+        private void LoadPrefab()
+        {
+            var outputDefinition = Prefab.Items.FirstOrDefault(x => x is OutputDefinitionSwitch);
+
+            if (outputDefinition != null)
+            {
+                foreach (var item in outputDefinition.Inputs)
+                {
+                    AddOutput(item.Signature, item.UIComponent);
+                }
+            }
+
+            var inputDefinition = Prefab.Items.FirstOrDefault(x => x is InputDefinitionSwitch);
+
+            if (inputDefinition != null)
+            {
+                foreach (var item in inputDefinition.Outputs)
+                {
+                    AddOutput(item.Signature, item.UIComponent);
+                }
+            }
+        }
+
+        public override GroupBase OnSetGroup()
+        {
+            return Groups.Meta;
+        }
+    }
+}
